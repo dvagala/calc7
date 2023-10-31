0d97683faeac10d5dfc4309f891a0038a66f546f
Dominik Vagala <d.vagala@gmail.com>
2023-10-31 14:45:42 +0100

    m

---
+++ Example/Pods/Target Support Files/Pods-calc7_Tests/Pods-calc7_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_calc7_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_calc7_Tests
+@end
---
+++ Example/Pods/Target Support Files/calc7/calc7-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_calc7 : NSObject
+@end
+@implementation PodsDummy_calc7
+@end
